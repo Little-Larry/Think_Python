31e336c0a24ecfdf5bb8bcf410f7f8822bcb10a2
Lawrence R. Amlord <informlarry@gmail.com>
2014-04-12 15:38:53 +0800

    add code in section 4.1 TurtleWorld.
    Included is the following file:
    
    	mypolygon.py
    
    Thank you!
    							Lawrence X. Amlord
    							  Apr 12th, 2014
    							  Xi'an, China

---
+++ Study_Alone/Chapter_4-Case_study-interface_design/4.1-TurtleWorld/mypolygon.py
@@ -0,0 +1,25 @@
+# mypolygon.py
+# from Think Python section 4.1
+# edited by Lawrence X. Amlord
+# Apr 11th, 2014
+# Xi'an, China
+
+from swampy.TurtleWorld import *
+
+world = TurtleWorld()
+bob = Turtle()
+print bob
+
+fd(bob, 100)
+lt(bob)
+
+fd(bob, 100)
+lt(bob)
+
+fd(bob, 100)
+lt(bob)
+
+fd(bob, 100)
+lt(bob)
+
+wait_for_user()
